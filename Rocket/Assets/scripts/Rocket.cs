bb82a120bf0733286b6c0b3a387adc7cfe9179a8
Yannick <yannick.jacques940@gmail.com>
2018-08-24 16:38:52 -0400

    Faire l'animation de décollage de la fusée

---
+++ Rocket/Assets/scripts/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    private Rigidbody rigidBody;
+    [SerializeField] public float ForceUpward = 100;
+    [SerializeField] public float ForceToRight= 100;
+    [SerializeField] public float ForceToLeft = 100;
+    // Use this for initialization
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rigidBody.AddForce(new Vector3(0, ForceUpward, 0));
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            rigidBody.AddForceAtPosition(new Vector3(ForceToRight,0,0),new Vector3();
+        }
+	}
+}
